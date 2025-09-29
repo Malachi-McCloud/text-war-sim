875f1f902ed8e75609c1ab19e5cb6a3fa7591a9c
Malachi-McCloud <mwmccloud2@outlook.com>
2025-09-29 17:53:41 -0400

    Follow up started adding a full game state to allow for actual scenario playthrough

---
+++ game_state.c
@@ -0,0 +1,12 @@
+// game_state.c - defines the gamestate struct
+
+#include "game_state.h"
+#include <string.h>         // For memset()
+
+/* Initializes the GameState Struct
+Function sets all bytes in the struct to zero then manually assigns default values
+Ensures clean memory and avoids garbage values */
+void GameState_init(GameState* state) {
+    // Zero out the entire structs memory
+    
+}
